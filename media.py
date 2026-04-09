a81e48a02b00f985bda85f61b5232dce30cb5efc
lliviamartins <lliviasantospaz2019@gmail.com>
2026-04-09 18:49:58 -0300

    calculo de media

---
+++ media.py
@@ -0,0 +1,20 @@
+#Montem uma expressão aritmética em Python que calcule a média de 3 notas:
+
+n1=8.0
+n2=6.7
+n3=5.6
+
+media=(n1+n2+n3)/3
+
+print(f'Soma entre {n1}+{n2}+{n3} é igual a:{n1+n2+n3:.2f}')
+
+print(f'Sua média é:{media:.2f}')
+
+if media>=6:
+    print('Parabéns! APROVADO!')
+else:
+    print('Infelizmente, foi reprovado.')
+        
+
+
+
